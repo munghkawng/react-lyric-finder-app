08b2716d7577269b63697673957a636eb4afbd71
munghkawng <munghkawngaung3@gmail.com>
2022-12-22 20:28:50 +0630

    add Load More Button

--- src/components/ShowAllLyrics.jsx
+++ src/components/ShowAllLyrics.jsx
@@ -1,60 +1,41 @@
-import PaginateButton from "./PaginateButton";
 import { useState, useEffect } from "react";
 
 import Loading from "./Loading";
+import { Button, Stack } from "@chakra-ui/react";
 import axios from "axios";
 import { Link, useLocation } from "react-router-dom";
 import SearchPage from "./page/SearchPage";
-import Pagination from "@mui/material/Pagination";
-import Stack from "@mui/material/Stack";
 
 function ShowAllLyrics() {
   const location = useLocation();
-  const [totalPageCount, setTotalPageCount] = useState(0);
+
   const [songData, setSongData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [controller, setController] = useState({
     page: 1,
-    pageSize: 10,
+    pageSize: 6,
   });
-
-  // const nextPage = () => {
-  //   setPage((oldPage) => {
-  //     let nextPage = oldPage + 1;
-  //     if (nextPage > data.length - 1) {
-  //       nextPage = 0;
-  //     }
-  //     return nextPage;
-  //   });
-  // };
-
-  // const prevPage = () => {
-  //   setPage((oldPage) => {
-  //     let prevPage = oldPage - 1;
-  //     if (prevPage < 0) {
-  //       prevPage = data.length - 1;
-  //     }
-  //     return prevPage;
-  //   });
-  // };
-
-  const handleChange = (event, value) => {
-    console.log(event);
-    setController({ page: value });
-  };
+  const [hasMore, setHasMore] = useState(true);
 
   const getSongData = async () => {
-    const allLyricsUrl = `https://guitaristchord.com/api/songs?page=${controller.page}&${controller.pageSize}`;
+    const allLyricsUrl = `https://guitaristchord.com/api/songs?page=${controller.page}&page_size=${controller.pageSize}`;
+
     try {
+      setLoading(true);
       const response = await axios(allLyricsUrl);
       if (response.status === 200) {
         const { data } = response;
-        setSongData(data.data);
-        setTotalPageCount(data.last_page);
-        setLoading(false);
+        setSongData((preValue) => {
+          return [...new Set([...preValue, ...data.data])];
+        });
+        setController((prevData) => {
+          return { ...prevData, page: prevData.page + 1 };
+        });
+        setHasMore(data.data.length > 0);
       } else {
         throw new Error("Request Failed");
       }
+      setLoading(false);
     } catch (error) {
       console.log(error.response);
     }
@@ -62,12 +43,8 @@ function ShowAllLyrics() {
 
   useEffect(() => {
     getSongData();
-  }, [controller]);
-
-  // useEffect(() => {
-  //   if (loading) return;
-  //   setSongData(data[page]);
-  // }, [loading, page, data]);
+    // eslint-disable-next-line
+  }, []);
 
   if (location.state === null) {
     return (
@@ -78,60 +55,66 @@ function ShowAllLyrics() {
           </div>
 
           <ul className="list-group list-group-flush ">
-            {loading ? (
-              <Loading />
-            ) : (
-              songData.map((song, index) => {
-                return (
-                  <li className="list-group-item" key={song.id}>
-                    <div className="card mb-3 border-0 ">
-                      <div className="row g-0 align-items-center">
-                        <div className="col-md-1 col-1 d-flex">
-                          <span className="pe-4 text-muted my-auto">
-                            {index + 1}
-                          </span>
-                          <img
-                            src={require("./assets/logo192.png")}
-                            className="img-thumbnail"
-                            alt="..."
-                            style={{ width: "90px" }}
-                          />
-                        </div>
-                        <div className="col-md-8 col-8 ms-5">
-                          <div className="card-body">
-                            <Link
-                              to={`/lyric/${song.slug}/${song.summary}`}
-                              className="card-title h6  text-black"
-                            >
-                              {song.title}
-                            </Link>
-                            <Link
-                              to={`/artist/${song.tags[0].slug}`}
-                              className="card-text text-muted mt-1 d-block"
-                            >
-                              {song.summary}
-                            </Link>
-                          </div>
+            {songData.map((song, index) => {
+              return (
+                <li className="list-group-item" key={index}>
+                  <div className="card mb-3 border-0 ">
+                    <div className="row g-0 align-items-center">
+                      <div className="col-md-1 col-2 d-flex">
+                        <span className="pe-2 text-muted my-auto">
+                          {index + 1}
+                        </span>
+                        <img
+                          src={require("./assets/logo192.png")}
+                          className="img-thumbnail"
+                          alt="..."
+                          style={{ width: "50px" }}
+                        />
+                      </div>
+                      <div className="col-md-11 col-10">
+                        <div className="card-body">
+                          <Link
+                            to={`/lyric/${song.slug}/${song.summary}`}
+                            className="text-black"
+                          >
+                            {song.title}
+                          </Link>
+                          <Link
+                            to={`/artist/${song.tags[0].slug}`}
+                            className="text-muted mt-1 d-block"
+                          >
+                            {song.summary}
+                          </Link>
                         </div>
                       </div>
                     </div>
-                  </li>
-                );
-              })
-            )}
+                  </div>
+                </li>
+              );
+            })}
           </ul>
         </div>
-        <div>
-          <Stack spacing={4}>
-            <Pagination
-              count={totalPageCount}
-              size="large"
-              variant="outlined"
-              page={controller.page}
-              onChange={handleChange}
-            />
+        {loading && <Loading />}
+
+        {!loading && hasMore && (
+          <Stack
+            direction="row"
+            align="center"
+            size="lg"
+            justifyContent="center"
+            mb={4}
+          >
+            <Button
+              colorScheme="gray"
+              onClick={getSongData}
+              variant="solid"
+              size="md"
+              border="1px"
+            >
+              Load More
+            </Button>
           </Stack>
-        </div>
+        )}
       </>
     );
   } else {
